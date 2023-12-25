10252c28b6538d91f684f5799e57380b09ba43c7
drunklol <16104507+patricklol22@users.noreply.github.com>
2023-12-25 21:24:39 +0100

    updating keywords

--- src/views/layout/PageHeader.vue
+++ src/views/layout/PageHeader.vue
@@ -1,9 +1,9 @@
 
 <template>
-  <div class="px-5 sm:px-20  m-auto h-16 bg-surface-card border-b border-surface-border">
-    <div class="flex divide-x  py-3 items-center h-full font-semibold max-w-4xl m-auto">
+  <div class="px-5 sm:px-20 m-auto h-16 bg-surface-card border-b border-surface-border">
+    <div class="flex divide-x py-3 items-center h-full font-semibold max-w-4xl m-auto">
       <div class="flex justify-between w-full items-center">
-        <h1 class="text-2xl">UUI Generator</h1>
+        <h1 class="text-2xl">Generate UUIDs</h1>
         <FormButton @click="emit('darkModeToggled')" class="!p-1" variant="surface"  :aria-label="darkmode ? 'switch to dark mode' : 'switch to light mode'">
           <transition enter-from-class="opacity-0" leave-to-class="opacity-0" mode="out-in"
             enter-active-class="transition duration-200" leave-active-class="transition duration-200 ">
--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,6 @@ export default defineConfig((options) => {
 return {
   ssgOptions: {
     crittersOptions: {
-      preload: 'media'
     }
   },
 
